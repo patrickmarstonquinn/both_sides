6e4ccbd5010ac952e398f4c0242e6f3ede3acbaf
Raghu Betina <raghu@firstdraft.com>
2017-03-01 20:09:57 +0000

    Direct association between users and favorite_issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :favorite_issues,
+             :dependent => :destroy
+
   has_many   :issues,
              :dependent => :destroy
 
