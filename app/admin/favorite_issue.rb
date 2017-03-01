3170d951e8a29fc4f5364c49ea7f209a59109f4e
Raghu Betina <raghu@firstdraft.com>
2017-03-01 20:10:18 +0000

    Whitelist columns in FavoriteIssue for ActiveAdmin

--- app/admin/favorite_issue.rb
+++ app/admin/favorite_issue.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FavoriteIssue do
 
+ permit_params :issue_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
